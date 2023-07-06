9b63e8cac4ee9e9746d49edff7cbb2ffc0907de7
Rufus100Procent <Maxamadabdulahist@outlook.com>
2023-07-06 18:56:26 +0200

    trying to fix playbutton, to load mp3

--- src/main/java/AWS/File/hosting/API/MusicController.java
+++ src/main/java/AWS/File/hosting/API/MusicController.java
@@ -8,6 +8,8 @@ import AWS.File.hosting.Service.MusicService;
 import jakarta.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 @Controller
 public class MusicController {
 
@@ -39,7 +42,28 @@ public class MusicController {
     public String uploadForm() {
         return "uploadForm";
     }
-
+    @GetMapping("/songs")
+    @ResponseBody
+    public List<Song> getAllSongs() {
+        return songRepository.findAll();
+    }
+    @GetMapping("/songs/{id}/file")
+    public ResponseEntity<Resource> downloadSong(@PathVariable("id") Long id) throws IOException {
+        Optional<Song> songOptional = songRepository.findById(id);
+        if (songOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Song song = songOptional.get();
+        String filePath = song.getFilePath();
+        File file = new File(filePath);
+        Resource resource = new UrlResource(file.toURI());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(org.springframework.http.MediaType.valueOf(MediaType.APPLICATION_OCTET_STREAM));
+        headers.setContentDisposition(ContentDisposition.attachment().filename(file.getName()).build());
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
+    }
     @PostMapping("/upload")
     public String uploadSong(@RequestParam("file") MultipartFile file,
                              @RequestParam("songName") String songName,
