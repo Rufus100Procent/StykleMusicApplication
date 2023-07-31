eeca20a881ecd066836e684fe5bc6bd964d78873
Mohamed Ali <66412126+Rufus100Procent@users.noreply.github.com>
2023-07-31 15:46:15 +0200

    Add files via upload

--- src/test/java/Rift/Radio/ContextTest.java
+++ src/test/java/Rift/Radio/ContextTest.java
@@ -18,43 +18,31 @@ public class ContextTest  extends Tests{
     void beforeEach()
     {
         String sql = """
-            CREATE TABLE songs (
-                id INT PRIMARY KEY,
-                album TEXT,
-                artist_name TEXT,
-                file_path TEXT,
-                release_year INT,
-                song_name TEXT,
-                genre TEXT
-            );
+          
+                CREATE TABLE songs (
+                  id INT PRIMARY KEY,
+                  album TEXT,
+                  artist_name TEXT,
+                  file_path TEXT,
+                  release_year INT,
+                  song_name TEXT,
+                  genre TEXT
+              );
+              
 
             INSERT INTO songs (id, album, artist_name, file_path, release_year, song_name, genre) VALUES
-                (62, 'Rocks', 'aerosmith', './LocalStorage/MP3/Aerosmith - Back In The Saddle (Audio).mp3', 1976, 'back in the saddle', 'hard Rock'),
-                (102, 'Garage Inc.', 'Metallica', './LocalStorage/MP3/Metallica - Whiskey in the jar.mp3', 1998, 'Whiskey in the jar', 'Heavy Metal'),
-                (73, 'Backseat', 'Peer Günt', './LocalStorage/MP3/Peer Günt  Backseat.mp3', 1986, 'Peer Günt - Backseat', 'hard Rock'),
-                (63, 'Rocks', 'Billy Idol', './LocalStorage/MP3/Billy Idol - Rebel Yell (Official Music Video).mp3', 1983, 'Robel Yell', 'Rock/Pop'),
-                (61, 'Back in Black', 'AC DC', './LocalStorage/MP3/AC DC - Shoot To Thrill.mp3', 1980, 'Shoot to Trill', 'hard Rock'),
-                (60, 'The Razors Edge', 'AC DC', './LocalStorage/MP3/AC DC - Fire Your Guns (Official Audio).mp3', 1990, 'Fire you guns', 'hard Rock'),
-                (64, 'Metallica', 'Metallica', './LocalStorage/MP3/Enter Sandman (Remastered).mp3', 1991, 'Enter the sand', 'Metal'),
-                (66, 'Ride the Lightning', 'Metallica', './LocalStorage/MP3/For Whom The Bell Tolls (Remastered).mp3', 1984, 'For Whom the Bells Told', 'Metal'),
-                (67, 'Ride the Lightning', 'Metallica/Ray Davies', './LocalStorage/MP3/You Really Got Me.mp3', 1964, 'You Really Got Me', 'Hard Rock'),
-                (68, 'Bad to the Bone', 'George Thorogood', './LocalStorage/MP3/George Thorogood & The Destroyers - Bad To The Bone.mp3', 1982, 'Bad to the Bone', 'Hard Rock/Blues'),
-                (65, 'Eliminator', 'ZZ Top', './LocalStorage/MP3/ZZ Top Sharp Dressed Man.mp3', 1983, 'Sharp Dressed Man', 'Rock/Blues Rock'),
-                (69, 'Morrison Hotel', 'Doors/Crystal Method', './LocalStorage/MP3/Roadhouse Blues.mp3', 1970, 'RoadHouse Blues', 'Rock'),
-                (75, 'Drones', 'Muse', './LocalStorage/MP3/Psycho - Muse (Lyrics).mp3', 2015, 'Pyscho', 'hard Rock'),
-                (103, 'Crash and Burn', 'Pat Travers', './LocalStorage/MP3/Snortin’ Whiskey.mp3', 1980, 'Snorting Whiskey', 'Rock'),
-                (109, 'Power Up', 'AC DC', './LocalStorage/MP3/AC DC - Shot In The Dark (Official Audio).mp3', 2020, 'Shot in the dark', 'Power Up'),
-                (110, 'Back in Black', 'AC DC', './LocalStorage/MP3/AC DC - Hells Bells (Official Video).mp3', 1980, 'hells bells', 'hard Rock'),
-                (111, 'The Game', 'Queen', './LocalStorage/MP3/Queen - Another One Bites The Dust [Lyrics].mp3', 1980, 'another one bites the dust', 'Funck Rock'),
-                (72, 'Cherry Pie', 'Warrant', './LocalStorage/MP3/Cherry Pie.mp3', 1990, 'Cherry Pie', 'Klassisk rock'),
-                (70, 'Out For Blood', 'Valley Of Wolves', './LocalStorage/MP3/Chosen One - Valley of Wolves (LYRICS).mp3', 2018, 'Chosen One', 'Blue/Rock'),
-                (71, 'Machine Head', 'Deep Purple', './LocalStorage/MP3/Deep Purple - Smoke on the Water (Audio).mp3', 1972, 'Smoke On the Water', 'Klassisk rock');
+                (1, 'Rocks', 'aerosmith', './src/test/java/Rift/Radio/songMP3Test/Aerosmith - Back In The Saddle (Audio).mp3', 1976, 'back in the saddle', 'hard Rock'),
+                ( 2, 'Garage Inc.', 'Metallica', './src/test/java/Rift/Radio/songMP3Test/Metallica - Whiskey in the jar.mp3', 1998, 'Whiskey in the jar', 'Heavy Metal'),
+                ( 3, 'Bad to the Bone', 'George Thorogood', './src/test/java/Rift/Radio/songMP3Test/George Thorogood & The Destroyers - Bad To The Bone.mp3', 1982, 'Bad to the Bone', 'Hard Rock/Blues'),
+                ( 4, 'Eliminator', 'ZZ Top', './src/test/java/Rift/Radio/songMP3Test/ZZ Top Sharp Dressed Man.mp3', 1983, 'Sharp Dressed Man', 'Rock/Blues Rock'),
+                ( 5, 'Crash and Burn', 'Pat Travers', './src/test/java/Rift/Radio/songMP3Test/Snortin’ Whiskey.mp3', 1980, 'Snorting Whiskey', 'Rock'),
+                ( 6, 'Power Up', 'AC DC', './src/test/java/Rift/Radio/songMP3Test/AC DC - Shot In The Dark (Official Audio).mp3', 2020, 'Shot in the dark', 'Power Up')
             """;
         jdbcTemplate.update(sql);
     }
     @AfterEach
     void afterEach() {
-        jdbcTemplate.update("CREATE TABLE IF NOT EXISTS songs");
 
+        jdbcTemplate.update("DROP TABLE IF EXISTS songs CASCADE");
     }
 }
--- src/test/java/Rift/Radio/Tests.java
+++ src/test/java/Rift/Radio/Tests.java
@@ -1,51 +1,87 @@
 package Rift.Radio;
 
 import Rift.Radio.Model.Song;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import static java.util.Collections.addAll;
-
-
-
 public class Tests {
-    protected final ObjectMapper OM = new ObjectMapper();
-    protected final static String NEW_ARTIST_NAME = "George Thorogood";
-    protected final static String NAME_DUPLICATE = "Duplicate";
+    private final static String TEST_FILE_DIRECTORY = "src/test/java/Rift/Radio/songMP3Test/";
+
+    protected final static String TEST_MP31 = "AC DC - Shot In The Dark (Official Audio).mp3";
+    protected final static String TEST_MP32 = "Snortin’ Whiskey.mp3";
+    protected final static String TEST_MP33 = "Aerosmith - Back In The Saddle (Audio).mp3";
+    protected final static String TEST_MP34 = "Metallica - Whiskey in the jar.mp3";
+
+    protected final static String DUPLICATE_SONG = "back in the saddle";
     protected final static String NAME_NONEXISTENT = "Nonexistent";
-    protected final static String NEW_TRACK_NAME = " Bad to the Bone";
 
-    protected static Song SONG1 = new Song(62L, "back in the saddle", "aerosmith", "Rocks", "hard Rock", 1976, "./LocalStorage/MP3/Aerosmith - Back In The Saddle (Audio).mp3");
-    protected static Song SONG2 = new Song(102L, "Whiskey in the jar", "Metalica", " Garage Inc.", "Heavy Metal", 1998, "./LocalStorage/MP3/Metallica - Whiskey in the jar.mp3");
-    protected final static Song SONG3 = new Song(73L, "Peer Günt - Backseat", "Peer Günt", "Backseat", "hard Rock", 1986, "./LocalStorage/MP3/Peer Günt  Backseat.mp3");
+    protected static Song SONG1 = new Song("back in the saddle", "aerosmith", "Rocks", "hard Rock", 1976, TEST_FILE_DIRECTORY + TEST_MP33);
+    protected static Song SONG2 = new Song( "Whiskey in the jar", "Metalica", " Garage Inc.", "Heavy Metal", 1998, TEST_FILE_DIRECTORY + TEST_MP34);
+    protected static Song SONG3 = new Song("Snorting Whiskey", "Pat Travers", "Crash and Burn", "Rock", 1980, TEST_FILE_DIRECTORY + TEST_MP32);
+    protected static Song SONG4 = new Song( "Shot in the dark", "AC DC", "Power Up", "Klassisk rock", 2020, TEST_FILE_DIRECTORY + TEST_MP31);
 
-    protected  static HashMap<Long, Song> SONGS_MAP;
+    protected static List<String> SONG_NAME;
     protected static List<String> ARTIST_NAMES;
-    protected static List<Song> SONGS;
-
+    protected static List<String> ALBUM_NAME;
+    protected static List<String> GENRE;
+    protected static List<Integer> RELEASE_YEAR;
     @BeforeAll
     protected static void beforeAll() {
-        SONGS_MAP = new HashMap<>();
-        SONGS_MAP.put(SONG1.getId(), SONG1);
-        SONGS_MAP.put(SONG2.getId(), SONG2);
-        SONGS_MAP.put(SONG3.getId(), SONG3);
 
-        System.out.println(SONGS_MAP);
+
+        SONG_NAME = new ArrayList<>();
+        SONG_NAME.add(SONG1.getSongName());
+        SONG_NAME.add(SONG2.getSongName());
+        SONG_NAME.add(SONG3.getSongName());
+        SONG_NAME.add(SONG4.getSongName());
+
+
+        //Verify
+        System.out.println(SONG_NAME);
+
 
         ARTIST_NAMES = new ArrayList<>();
         ARTIST_NAMES.add(SONG1.getArtistName());
         ARTIST_NAMES.add(SONG2.getArtistName());
         ARTIST_NAMES.add(SONG3.getArtistName());
+        ARTIST_NAMES.add(SONG4.getArtistName());
 
-
+        //Verify
         System.out.println(ARTIST_NAMES);
-        SONGS = new ArrayList<>(SONGS_MAP.values());
-    }
 
+
+        ALBUM_NAME = new ArrayList<>();
+        ALBUM_NAME.add(SONG1.getAlbum());
+        ALBUM_NAME.add(SONG2.getAlbum());
+        ALBUM_NAME.add(SONG3.getAlbum());
+        ALBUM_NAME.add(SONG4.getAlbum());
+
+        //Verify
+        System.out.println(ALBUM_NAME);
+
+
+
+        GENRE = new ArrayList<>();
+        GENRE.add(SONG1.getGenre());
+        GENRE.add(SONG2.getGenre());
+        GENRE.add(SONG3.getGenre());
+        GENRE.add(SONG4.getGenre());
+
+        //Verify
+        System.out.println(GENRE);
+
+
+        RELEASE_YEAR = new ArrayList<>();
+        RELEASE_YEAR.add(SONG1.getReleaseYear());
+        RELEASE_YEAR.add(SONG2.getReleaseYear());
+        RELEASE_YEAR.add(SONG3.getReleaseYear());
+        RELEASE_YEAR.add(SONG4.getReleaseYear());
+
+        //Verify
+        System.out.println(RELEASE_YEAR);
+
+    }
 
 }
