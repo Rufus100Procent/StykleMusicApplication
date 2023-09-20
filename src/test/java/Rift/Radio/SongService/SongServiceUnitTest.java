5be62660fd52e4adda85bbd98fb465ca8d87af8a
Rufus100Procent <Maxamadabdulahist@outlook.com>
2023-09-20 13:50:52 +0200

    test

--- src/test/java/Rift/Radio/SongService/SongServiceUnitTest.java
+++ src/test/java/Rift/Radio/SongService/SongServiceUnitTest.java
@@ -12,7 +12,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
 import java.io.IOException;
