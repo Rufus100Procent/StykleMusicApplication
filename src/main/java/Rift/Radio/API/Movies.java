5a38a338471caf5500a36e2fe45b435692945eb7
Rufus100Procent <Maxamadabdulahist@outlook.com>
2023-09-28 13:35:00 +0200

    taking to next level, added movies, tvshows, goal = all in one

---
+++ src/main/java/Rift/Radio/API/Movies.java
@@ -0,0 +1,15 @@
+package Rift.Radio.API;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Movies")
+public class Movies {
+
+    @GetMapping
+    public String moviePage(){
+        return "Movies/movies";
+    }
+}
